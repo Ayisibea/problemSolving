51ed4c49623d93b8d7dd81cdd6c76248fa2a2def
Ayisibea <154318314+Ayisibea@users.noreply.github.com>
2024-09-19 15:44:26 +0000

    Add files via upload

---
+++ sortColours.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def sortColors(self, nums):
+        for j in range(len(nums)):
+         for i in range(len(nums)-1):
+            if nums[i]>nums[i+1]:
+                nums[i],nums[i+1]=nums[i+1],nums[i]
